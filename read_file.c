6be0bdab6374a2f854478429c2cc469618e1d368
lemtzas <lemtzas@gmail.com>
2012-12-02 15:43:56 -0800

    readFile loads into Memory object now.

--- read_file.c
+++ read_file.c
@@ -1,37 +1,43 @@
 #include "read_file.h"
 
 char* readFile(char* file,Memory *memory) {
+    //printf("loading %s\n",file);
     FILE *fp;
     int i=0;
     char* string;
-    char* temp;
+    Register* temp;
     /* open the file */
     fp = fopen(file, "r");
-    if (fp == NULL) {
-         printf("Could not open file.\n");
-         exit(0);
-    }
+    if (fp == NULL) return "Could not open file.";
 
-    while (fscanf(fp, "%x", &temp) == 1){
-        strcat(string, temp);
+    //printf("loading %s %x\n",file,fp);
+    while (fscanf(fp, "%04x", &temp) == 1){
+        memory->location[i] = temp;
         i++;
     }
+    //printf("loading %s\n\n",file);
     /* close the file */
     fclose(fp);
 
-    return string;
+    return 0;
 }
 
 
 ///Test driver for readFile
-int main()
-{
-    Memory memory;
-    memory_initialize(&memory);
-
-    readFile("read_file_test.txt",&memory);
-
-
-    int i = 0;
-    for()
-}
+//int main()
+//{
+//    Memory memory;
+//    memory_initialize(&memory);
+//
+//    readFile("read_file_test.txt",&memory);
+//
+//
+//    int i = 0;
+//    for(i = 0; i < 100; i++)
+//    {
+//        if( memory.location[i] )
+//        {
+//            printf("%04x\n",memory.location[i]);
+//        }
+//    }
+//}
